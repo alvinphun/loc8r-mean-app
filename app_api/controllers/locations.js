003c2f7b69b6dd8efad154b7f64a195e995eb338
Alvin Phun <alvin.phun@gmail.com>
2016-05-12 23:35:21 -0400

    Added locationid and reviewid to live database

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+var sendJSONresponse = function(res, status, content) {
+	res.status(status);
+	res.json(content);
+};
+
+/* GET a location by the id */
+module.exports.locationsReadOne = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
+};
---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+var sendJSONResponse = function(res, status, content) {
+	res.status(status);
+	res.json(content);
+};
+
+module.exports.reviewsReadOne = function (req, res) {
+	if (req.params && req.params.locationid && req.params.reviewid) {
+		Loc
+			.findById(req.params.locationid)
+			.select('name reviews')
+			.exec(
+				function(err, location) {
+					var response, review;
+					if (!location) {
+						sendJSONResponse(res, 404, {"message" : "locationid not found"});
+						return;
+					} else if (err) {
+						sendJSONResponse(res, 200, err);
+						return;
+					}
+					if (location.reviews && location.reviews.length > 0) {
+						review = location.reviews.id(req.params.reviewid);
+						if (!review) {
+							sendJSONResponse(res, 404, {
+								"message": "reviewid not found"
+							});
+						} else {
+							response = {
+								location: {
+									name: location.name,
+									id: req.params.locationid
+								},
+								review: review
+							};
+							sendJSONResponse(res, 200, response);
+						}
+					} else {
+						sendJSONResponse(res, 404, {
+							"message": "No reviews found"
+						});
+					}
+				});
+	} else {
+		sendJSONResponse(res, 404, {
+			"message": "Not found, locationid and reviewid are both required"
+		});
+	}
+};
