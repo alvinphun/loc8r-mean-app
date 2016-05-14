cf98355b4af31ea11a82fb62e68bb50a71463085
Alvin Phun <alvin.phun@gmail.com>
2016-05-14 13:53:28 -0400

    Added renderHomepage

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,57 +1,42 @@
-/* GET 'home' page */
-module.exports.homelist = function(req, res){
+var request = require('request');
+var apiOptions = {
+  server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://guarded-ridge-36463.herokuapp.com";
+}
+
+var renderHomepage = function(req, res, responseBody) {
 res.render('locations-list', { 
 	title: 'Loc8r - find a place to work with wifi',
 	pageHeader: {
 		title: 'Loc8r',
 		strapline: 'Find places to work with wifi near you!'
-		},
+	},
 	sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-		locations: [{
-			name: 'Starcups',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 3,
-			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-			coords: {lat: 51.455041, lng: -0.9690884},
-			openingTimes: [{
-				days: 'Monday - Friday',
-				opening: '7:00am',
-				closing: '7:00pm',
-				closed: false
-			}, {
-				days: 'Saturday',
-				opening: '8:00am',
-				closing: '5:00pm',
-				closed: false
-			}, {
-				days: 'Sunday',
-				closed: true
-			}], 
-			reviews: [{
-				author: 'Simon Holmes',
-				rating: 5,
-				timestamp: '16 July 2013',
-				reviewText: 'What a great place. I can\'t say enough good things about it.'
-			}, {
-				author: 'Charlie Chaplin',
-				rating: 3, 
-				timestamp: '16 June 2013',
-				reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-			}]
-		},{
-			name: 'Cafe Hero',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 4,
-			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-			distance: '200m'
-		},{
-			name: 'Burger Queen',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 2,
-			facilities: ['Food', 'Premium wifi'],
-			distance: '250m'
-		}]
+	locations: responseBody
 	});
+};
+
+/* GET 'home' page */
+module.exports.homelist = function(req, res){
+	var requestOptions, path;
+	path='/api/locations';
+	requestOptions = {
+		url : apiOptions.server + path,
+		method: "GET",
+		json: {},
+		qs: {
+			lng: -0.7992599,
+			lat: 51.378091,
+			maxDistance: 20
+		}
+	};
+	request(
+		requestOptions,
+		function(err, response, body) {
+			renderHomepage(req, res, body);
+		});
 };
 
 /* GET 'Location info' page */
